1979d4f535f58b4025f613a9833bea13b583f34f
khainee <118953358+khainee@users.noreply.github.com>
2022-11-29 16:43:37 +0630

    Update wserver.py

--- wserver.py
+++ wserver.py
@@ -684,7 +684,7 @@ async def set_priority(request):
 @routes.get('/')
 async def homepage(request):
 
-    return web.Response(text="<h1>See mirror-leech-telegram-bot <a href='https://www.github.com/anasty17/mirror-leech-telegram-bot'>@GitHub</a> By <a href='https://github.com/anasty17'>Anas</a></h1>", content_type="text/html")
+    return web.Response(text="<h1>See mirror-leech-telegram-bot <a href='https://github.com/LwinMinOo558/ShanMirror'>@GitHub</a> By <a href='https://t.me/BotTutorialsMM'>Bot Tutorials</a></h1>", content_type="text/html")
 
 async def e404_middleware(app, handler):
 
